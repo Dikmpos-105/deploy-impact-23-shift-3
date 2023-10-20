984bea25de08cf72b244effe4029063421a68a28
Patricia <patricia@wintrebert.ch>
2023-10-20 18:54:40 +0200

    compare-values python file

--- compare_values.py
+++ compare_values.py
@@ -9,89 +9,63 @@ import requests
 from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 # Load the pre-trained BERT model and tokenizer
 model_name = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
-api_candidates = 'https://django-backend-shift-enter-u53fbnjraa-oe.a.run.app/api/values/'
-api_companies = 'https://django-backend-shift-enter-u53fbnjraa-oe.a.run.app/api/values/'
+#Replace this part to to the connexion with the DB
+cv_folder_path = "C:/Users/PatriciaWintrebert/Womenplusplus/extractenv/app/data/resume/"  
+job_description_csv = "IT_entry_swiss_jobs_200.csv"  
+job_descriptions_df = pd.read_csv(job_description_csv)
 
-try:
-    response = requests.get(api_candidates)
+# Concatenation of columns to create free text to analyze
+job_descriptions_df["description"] = job_descriptions_df["job_title"] + " " + job_descriptions_df["skills"]
 
-    # Check if request is successful
-    if response.status_code == 200:
-        data = response.json()
-
-        # Request values from the API for candidates
-        candidate_values = [item['list_values_name'] for item in data]
-
-    else:
-        print(f"Request for candidates failed, code: {response.status_code}")
-
-except Exception as e:
-    print(f"Request for candidates failed: {str(e)}")
-
-try:
-    response = requests.get(api_companies)
-
-    # Check if request is successful
-    if response.status_code == 200:
-        data = response.json()
-
-        # Request values from the API for companies
-        company_values = [item['list_values_name'] for item in data]
-
-    else:
-        print(f"Request for companies failed, code: {response.status_code}")
-
-except Exception as e:
-    print(f"Request for companies failed: {str(e)}")
-
-# List to store similarity scores
 similarities = []
 
 try:
-    # Iterate through the candidate_values
-    for candidate_val in candidate_values:
-        print(f"Processing candidate value: {candidate_val}")
+
+    for cv_file in glob.glob(os.path.join(cv_folder_path, "*.txt")):
+        print(f"Processing CV file: {cv_file}")
 
         try:
-            # Tokenize and encode the candidate value
-            candidates_token = tokenizer(candidate_val, padding=True, truncation=True, return_tensors="pt")
+            with open(cv_file, "r", encoding="utf-8") as file:
+                cv_text = file.read()
 
-            # Iterate through the company_values
-            for company_val in company_values:
-                print(f"Processing company value: {company_val}")
+            # Tokenize and encoding
+            cv_tokens = tokenizer(cv_text, padding=True, truncation=True, return_tensors="pt")
 
-                # Tokenize and encode the company value
-                company_token = tokenizer(company_val, padding=True, truncation=True, return_tensors="pt")
+            for index, job_description_row in job_descriptions_df.iterrows():
+                job_description_text = job_description_row["description"]
 
-                # Get BERT embeddings for the sentences
+                job_description_tokens = tokenizer(job_description_text, padding=True, truncation=True, return_tensors="pt")
+
+                # Get embeddings BERT of phrases
                 with torch.no_grad():
-                    candidate_output = model(**candidates_token)
-                    company_output = model(**company_token)
+                    cv_outputs = model(**cv_tokens)
+                    job_description_outputs = model(**job_description_tokens)
 
-                # Get sentence embeddings (using the output from the embedding layer)
-                cv_embeddings = candidate_output.last_hidden_state.mean(dim=1)
-                job_description_embeddings = company_output.last_hidden_state.mean(dim=1)
+                # Get embedding
+                cv_embeddings = cv_outputs.last_hidden_state.mean(dim=1)
+                job_description_embeddings = job_description_outputs.last_hidden_state.mean(dim=1)
 
-                # Calculate cosine similarity between sentences
+                # Calculate similarity
                 similarity = cosine_similarity(cv_embeddings, job_description_embeddings)
 
-                # Add the similarity score and the candidate and company values to the list
-                similarities.append((candidate_val, company_val, similarity[0][0]))
+                # Add similarity score
+                similarities.append((os.path.basename(cv_file), job_description_row["job_title"], similarity[0][0]))
 
         except Exception as e:
-            print(f"Error processing candidate value {candidate_val} or company value {company_val}: {str(e)}")
+            print(f"Error processing CV file {cv_file}: {str(e)}")
 
-    # Sort candidates by similarity score (from most similar to least similar)
+    # Sort candidates by similarity
     similarities.sort(key=lambda x: x[2], reverse=True)
 
-    # Display the sorted list of candidates and their best company value matches
-    for candidate_val, company_val, score in similarities:
-        print(f"Candidate Value: {candidate_val}, Company Value: {company_val}, Similarity Score: {score}")
+    # Display candidates and similarity
+    for cv_file, job_title, score in similarities:
+        print(f"CV File: {cv_file}, Job Title: {job_title}, Similarity Score: {score}")
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
